28ebc3eb41dcb5f02e0a565613bd92b00a0fc6b9
Shapsty <shapsty@gmail.com>
2022-06-09 14:43:22 -0500

    added ModID and MODNAME to SeedMod.java Also fixed cancerous code design

--- src/main/java/com/Mod/SeedMod/SeedMod.java
+++ src/main/java/com/Mod/SeedMod/SeedMod.java
@@ -10,21 +10,19 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = SeedMod.MODID, name = SeedMod.NAME, version = SeedMod.VERSION)
 public class SeedMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "SeedMod";
+    public static final String NAME = "Seed Mod";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
         // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
