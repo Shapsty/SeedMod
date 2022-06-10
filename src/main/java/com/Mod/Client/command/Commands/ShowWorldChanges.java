9215bf0f509142d8670ce9ce4b39dc28fa0101ab
Shapsty <shapsty@gmail.com>
2022-06-10 18:02:06 -0500

    added mc_biome to the gradle
    Started onCommand method in ShowWorldChanges.java where the execution of the command will take place

--- src/main/java/com/Mod/Client/command/Commands/ShowWorldChanges.java
+++ src/main/java/com/Mod/Client/command/Commands/ShowWorldChanges.java
@@ -5,9 +5,15 @@ import com.Mod.Client.command.Command;
 @Command.Declaration(name = "SeedOverlay", syntax ="SO [seed]")
 public class ShowWorldChanges extends Command {
     //main seed overlay command thing
+    public void onCommand(String command, String[] message){
+        String main = message[0];
+        String seed = message[1];
+
+    }
     private static int showChanges(){
         int blocks = 0;
-        // find out what chunk the player is in
+        // next we need to find out what chunk the player is in
+
         return blocks;
     }
     private static int returnPlayerDim(){
