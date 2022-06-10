b4228be84da949e5b6886d877438390f28f09354
Shapsty <shapsty@gmail.com>
2022-06-10 18:11:04 -0500

    changed returnPLayerDim() to read easier and work for TerrainGenerator and BiomeSource

--- src/main/java/com/Mod/Client/command/Commands/ShowWorldChanges.java
+++ src/main/java/com/Mod/Client/command/Commands/ShowWorldChanges.java
@@ -1,6 +1,10 @@
 package com.Mod.Client.command.Commands;
 
 import com.Mod.Client.command.Command;
+import com.seedfinding.mcbiome.biome.Biome;
+import com.seedfinding.mcbiome.biome.Biomes;
+import com.seedfinding.mcbiome.source.BiomeSource;
+import com.seedfinding.mcterrain.TerrainGenerator;
 
 @Command.Declaration(name = "SeedOverlay", syntax ="SO [seed]")
 public class ShowWorldChanges extends Command {
@@ -11,18 +15,27 @@ public class ShowWorldChanges extends Command {
 
     }
     private static int showChanges(){
+        //BiomeSource biomeSource = BiomeSource.of()
+        //TerrainGenerator generator = TerrainGenerator
+
         int blocks = 0;
         // next we need to find out what chunk the player is in
-
         return blocks;
     }
-    private static int returnPlayerDim(){
-        int Dimension = 0;
+    private static String returnPlayerDim(){
+        String playerDim = "";
 
-        if (mc.player.dimension == -1){
-            Dimension = -1;
+        switch (mc.player.dimension){
+            case 0:
+                playerDim = "Overworld";
+                break;
+            case -1:
+                playerDim = "Nether";
+                break;
+            case 1:
+                playerDim = "End";
+                break;
         }
-
-        return Dimension;
+        return playerDim;
     }
 }
