2da94529a4fffd8dae4beb649f6f3bc20866f40e
Shapsty <shapsty@gmail.com>
2022-06-12 17:28:09 -0500

    I believe I got mixins working we about to find tf out

--- src/main/java/com/Mod/mixin/SeedModMixinLoader.java
+++ src/main/java/com/Mod/mixin/SeedModMixinLoader.java
@@ -1,4 +1,47 @@
 package com.Mod.mixin;
 
-public class SeedModMixinLoader {
+import com.Mod.Client.SeedMod;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.Mixins;
+
+import java.util.Map;
+
+@IFMLLoadingPlugin.Name(SeedMod.NAME)
+@IFMLLoadingPlugin.MCVersion("1.12.2")
+public class SeedModMixinLoader implements IFMLLoadingPlugin {
+    private static boolean isObfuscatedEnvironment = false;
+
+    public SeedModMixinLoader() {
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.SeedMod.json");
+        MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+        isObfuscatedEnvironment = (boolean) data.get("runtimeDeobfuscationEnabled");
+    }
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
 }
