1cfa2d99f2792f6a8c60bcc87485cb2ffb8e49a7
Shapsty <shapsty@gmail.com>
2022-06-12 19:52:47 -0500

    Updated README.md issues as I Solved them
    Added MixinsGUIMainMenu back

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 Mod for finding changes to the world compared to the seed
 
 # Issues
-1. For some reason forge currently does not recognize the jar as a mod. 
- UPDATE: I think I found the reason, I think you have to force load it as a mod and have a IFMLplugin
- this is fine because we need that for rendering anyways.
-2. this might be related to issue one, the game now whenever 
-I try to launch forge with the mod it throws a java runtime error.
-3. WE NEED TO ADD MIXINS FOR RENDING AND STUFF LIKE THAT
+1. For some reason forge currently does not recognize the jar as a mod.
+in the mods tab specifically the test GUI mixin works just fine.
+2. WE NEED TO ADD MIXINS FOR RENDING AND STUFF LIKE THAT
--- src/main/java/com/Mod/mixin/mixins/MixinGuiMainMenu.java
+++ src/main/java/com/Mod/mixin/mixins/MixinGuiMainMenu.java
@@ -1,18 +1,19 @@
 package com.Mod.mixin.mixins;
 
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.At;
+
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.text.TextFormatting;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin({GuiMainMenu.class})
 public class MixinGuiMainMenu extends GuiScreen {
-    @Inject(method = {"drawScreen"}, at = {@At("TAIL")}, cancellable = true, remap = false)
-    public void drawText(CallbackInfo ci){
-        mc.fontRenderer.drawStringWithShadow(TextFormatting.GRAY + "SeedMod", 1,1 , 0xFFFFFF);
+    @Inject(method = {"drawScreen"}, at = {@At("TAIL")}, cancellable = true)
+    public void drawText(CallbackInfo ci) {
+        mc.fontRenderer.drawStringWithShadow( TextFormatting.GOLD + "SEED MOD!!!!", 1, 1, 0xffffff);
     }
 
 }
