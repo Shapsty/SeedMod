817d5f302032763a60389232ad237015d26ccaab
Shapsty <shapsty@gmail.com>
2022-06-14 19:56:21 -0500

    for some reason showChanges Doe not loop through the second for loop it loops the first for loop 175 times

--- src/main/java/com/Mod/Client/command/Commands/ShowWorldChanges.java
+++ src/main/java/com/Mod/Client/command/Commands/ShowWorldChanges.java
@@ -3,13 +3,16 @@ package com.Mod.Client.command.Commands;
 import com.Mod.Client.command.Command;
 
 import com.Mod.api.util.chat.messages;
+import com.Mod.api.util.map.SimpleBlockMap;
 import com.seedfinding.mcbiome.biome.Biome;
 import com.seedfinding.mcbiome.biome.Biomes;
 import com.seedfinding.mcbiome.source.BiomeSource;
 import com.seedfinding.mccore.state.Dimension;
 import com.seedfinding.mccore.version.MCVersion;
 import com.seedfinding.mcterrain.TerrainGenerator;
+import net.minecraft.util.text.*;
 
+import net.minecraft.block.Block;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.chunk.Chunk;
@@ -29,7 +32,7 @@ public class ShowWorldChanges extends Command {
 
     //main seed overlay command thing
     @Override
-    public void onCommand(String command, String[] message){
+    public void onCommand(String command, String[] message) {
         String main = message[0];
         String seed = message[1];
 
@@ -37,7 +40,7 @@ public class ShowWorldChanges extends Command {
         final Chunk chunk = mc.player.getEntityWorld().getChunk(center);
         final ChunkPos chunkPos = chunk.getPos();
 
-        if (main == null || seed == null){
+        if (main == null || seed == null) {
             messages.sendCommandMessage(this.getSyntax(), true);
             return;
         }
@@ -46,14 +49,18 @@ public class ShowWorldChanges extends Command {
         messages.sendCommandMessage(String.valueOf(center), true);
         messages.sendCommandMessage(String.valueOf(chunk), true);
         messages.sendCommandMessage(String.valueOf(chunkPos), true);
+        showChanges("-4684036758601247941");
     }
-    private static int showChanges(String seed){
+
+    private static int showChanges(String seed) {
         // before we implement rendering we should just print the missing blocks and their coordinates to the chat
+        messages.sendCommandMessage("1.12 Seed Overlay init", true);
 
         BiomeSource biomeSource = BiomeSource.of(returnPlayerDim(), MCVersion.v1_12_2, Long.parseLong(seed));
         TerrainGenerator generator = TerrainGenerator.of(returnPlayerDim(), biomeSource);
         BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
         final BlockPos center = new BlockPos(mc.player.getPosition());
+        final SimpleBlockMap map = new SimpleBlockMap(MCVersion.v1_12_2, returnPlayerDim(), Biomes.PLAINS);
         //final WorldChunk chunk = mc.player.getWorld().getChunk(center.getX() >> 4, center.getZ() >> 4); This doesn't work for 1.12 but this is what we are trying to implement
         final Chunk chunk = mc.player.getEntityWorld().getChunk(center);
         final ChunkPos chunkPos = chunk.getPos();
@@ -61,11 +68,37 @@ public class ShowWorldChanges extends Command {
         //this also doesnt work for 1.12 but this is what we are trying to implement
         //Map<Box, String> boxes = new HashMap<>();
         int blocks = 0;
-        for (int x = chunkPos.getXStart(); x <= chunkPos.getXEnd(); x++){
-            for (int z = chunkPos.getZStart(); x <= chunkPos.getZEnd(); z++){
-
+        messages.sendCommandMessage("1.12 Seed Overlay Started", true);
+        for (int x = chunkPos.getXStart(); x <= chunkPos.getXEnd(); x++) {
+            messages.sendCommandMessage("first for", true);
+            messages.sendCommandMessage(String.valueOf(x), true);
+            for (int z = chunkPos.getZStart(); x <= chunkPos.getZEnd(); z++) {
+                messages.sendCommandMessage("Second for", true);
+                messages.sendCommandMessage("Set Pos 123", true);
+                final com.seedfinding.mccore.block.Block[] column = generator.getColumnAt(x, z);
+                final Biome biome = biomeSource.getBiome(x, 0, z);
+                map.setBiome(biome);
+                messages.sendCommandMessage("Set Biome", true);
+                for (int y = 0; y < column.length; y++) {
+                    messages.sendCommandMessage("Third for", true);
+                    mutable.setPos(x, y, z);
+                    messages.sendCommandMessage("Set Y", true);
+                    final Block terrainBlock = chunk.getBlockState(mutable).getBlock();
+                    String terrainBlockName = Block.REGISTRY.getNameForObject(terrainBlock).getPath();
+                    messages.sendCommandMessage("Set Y 5", true);
+                    if (map.get(terrainBlock) == column[y].getId()) {
+                        continue;
+                    }
+                    messages.sendCommandMessage("Block at " + mutable.toString() + " is " + terrainBlockName, true);
+                    blocks++;
+                    messages.sendCommandMessage("incremented", true);
+                }
             }
         }
+        if (blocks > 0) {
+            messages.sendCommandMessage("There are " + String.valueOf(blocks) + " blocks that are different from the seed", true);
+        }
+        messages.sendCommandMessage("1.12 Seed Overlay Complete", true);
         return blocks;
     }
     private static Dimension returnPlayerDim(){
@@ -73,7 +106,6 @@ public class ShowWorldChanges extends Command {
 
         switch (mc.player.dimension){
             case 0:
-                playerDim = Dimension.OVERWORLD;
                 break;
             case -1:
                 playerDim = Dimension.NETHER;
